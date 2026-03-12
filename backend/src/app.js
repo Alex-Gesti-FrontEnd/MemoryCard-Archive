069cb3e9fab2de1d4e193b4bec667c35146cdbff
AlexGesti <alexgesti001@gmail.com>
2026-03-12 20:28:27 +0100

    chore: app.js update

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 import gamesRouter from './routes/games.routes.js';
 import remindersRouter from './routes/reminders.routes.js';
 
@@ -13,7 +14,12 @@ app.use(express.json());
 app.use('/api/games', gamesRouter);
 app.use('/api/reminders', remindersRouter);
 
-app.get('/', (req, res) => res.json({ ok: true, message: 'API backend funcionando' }));
+const angularDistPath = path.join(process.cwd(), 'backend/dist/angular8/browser');
+app.use(express.static(angularDistPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(angularDistPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`API arrancada en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`API + Frontend arrancados en http://localhost:${PORT}`));
